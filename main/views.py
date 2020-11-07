cc45a2823a699c6ab8fd84f8af352cc3ed0e9138
Thomas Dahlberg <thomas.dahlberg8@gmail.com>
2020-11-06 21:33:36 -0500

    initializes views, creates urls.py

--- main/views.py
+++ main/views.py
@@ -1,3 +1,56 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+from .models import Profile, Site, TimePunch, Session
+
+# Site Views
+
+def site_index(request):
+    sites = Sites.objects.all().order_by('-id')
+    return JsonResponse(sites)
+
+def add_site(request):
+    pass
+
+def update_site(request):
+    pass
+
+def get_site(request, site_id):
+    site = Sites.objects.get(id=site_id)
+    return JsonResponse(site)
+
+# Session Views
+
+def session_index(request):
+    sessions = Session.objects.all().order_by('-id')
+
+def add_session(request):
+    pass
+
+def update_session(request):
+    pass
+
+def get_session(request, session_id):
+    session = Session.objects.get(id=site_id)
+    return JsonResponse(session)
+
+# Profile Views
+
+def profile_index(request):
+    employees = Profile.objects.all().order_by('-id')
+    JsonResponse(employees)
+
+def update_profile(request):
+    pass
+
+def get_profile(request, profile_id):
+    user = Profile.objects.get(id=profile_id)
+    JsonResponse(user)
+
+# Authentication and Signup
+
+def sign_up(request):
+    pass
